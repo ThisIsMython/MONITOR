942b402536460aa568d76dafc46df37f2d2db099
zqh <32186337+ThisIsMython@users.noreply.github.com>
2018-06-05 22:11:02 +0800

    Update cpu.py

--- cpu.py
+++ cpu.py
@@ -25,7 +25,33 @@ def CPUinfo():
                 if len(line.split(':')) == 2:
                     procinfo[line.split(':')[0].strip()] = line.split(':')[1].strip()
                 else:
-                    procinfo[line.split(':')[0].strip()] = ''
+                    procinfo[line.split(':')[0].strip()] 
+ 
+    return CPUinfo
+ 
+ def MEMinfo():
+    ''' Return the information in /proc/CPUinfo
+    as a dictionary in the following format:
+    CPU_info['proc0']={...}
+    CPU_info['proc1']={...}
+    '''
+    CPUinfo=OrderedDict()
+    procinfo=OrderedDict()
+ 
+    nprocs = 0
+    with open('/proc/cpuinfo') as f:
+        for line in f:
+            if not line.strip():
+                # end of one processor
+                CPUinfo['proc%s' % nprocs] = procinfo
+                nprocs=nprocs+1
+                # Reset
+                procinfo=OrderedDict()
+            else:
+                if len(line.split(':')) == 2:
+                    procinfo[line.split(':')[0].strip()] = line.split(':')[1].strip()
+                else:
+                    procinfo[line.split(':')[0].strip()] 
  
     return CPUinfo
  
